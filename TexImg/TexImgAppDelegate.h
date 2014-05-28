f73e84195ad4c54dc0f2e7936d3b50e68901ca2d
awalin sopan <awalinnabila@gmail.com>
2014-05-28 11:10:00 -0400

    Initial Commit

---
+++ TexImg/TexImgAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TexImgAppDelegate.h
+//  TexImg
+//
+//  Created by Sopan, Awalin on 5/28/14.
+//  Copyright (c) 2014 __mstr__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TexImgAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TexImg/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TexImg
+//
+//  Created by Sopan, Awalin on 5/28/14.
+//  Copyright (c) 2014 __mstr__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TexImgAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TexImgAppDelegate class]));
+    }
+}
